04cbf6975106651f5a2c68babd63c131a66b3dd4
cziur <crisamziur@gmail.com>
2024-11-25 10:28:42 -0800

    actually works now with steamurl

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
-document.getElementById("getStatButton").addEventListener("click", function () {
-  let url = `https://brawlhalla.fly.dev/v1/ranked/steamurl?steam_url=https://steamcommunity.com/id/${steamName}/`;
-  let steamName = document.querySelector("input").value;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
-});
+(function () {
+  const statsButton = document.getElementById("getStatButton");
+  statsButton.addEventListener("click", function () {
+    const steamNameInput = document.getElementById("inputSteamName");
+    const url = `https://brawlhalla.fly.dev/v1/ranked/steamurl?steam_url=https://steamcommunity.com/id/${steamNameInput.value}/`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(data);
+        document.querySelector("h2").innerHTML = res.data.rating;
+        document.querySelector("h3").innerHTML = res.data.peak_rating;
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+      });
+  });
+})();
